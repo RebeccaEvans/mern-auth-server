1b44f6030d61d4754c994a8377f14245d002f27b
RebeccaEvans <evans.rebeccaj@gmail.com>
2020-01-23 16:08:38 -0800

    added db and other modules

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,66 @@
+require('dotenv').config()
 let router = require('express').Router()
+let db = require('../models')
+let jwt = require('jsonwebtoken')
 
 // POST /auth/login (find and validate user; send token)
 router.post('/login', (req, res) => {
-  res.send('STUB POST /auth/login')
+  console.log(req.body)
+  // look up user
+  db.User.findOne({email: req.body.email})
+  .then(user => {
+    // make sure user exists and has a password
+    if (!user || !user.password) {
+      return res.status(404).send({message: 'User not found'})
+    }
+    //good, now chec password
+    if (!user.isValidPassword(req.body.password)) {
+      return res.status(401).send({message: 'password is incorrect'})
+    }
+
+    // good user - issue token
+    let token = jwt.sign(user.toJSON(), process.env.JWT_SECRET, {
+      expiresIn: 60 //in seconds
+    })
+    res.send({token})
+})
+  .catch(err =>{
+    console.log('error in POST/auth/login', err)
+    res.status(503).send({message: 'database or server-side error'})
+  })
+
 })
 
 // POST to /auth/signup (create user; generate token)
 router.post('/signup', (req, res) => {
-  res.send('STUB POST /auth/login')
+  console.log(req.body)
+  //look up user and make sure not a duplicate
+  db.User.findOne({email: req.body.email})
+    .then(user =>{
+      //if the user exists, do NOT let them create another account
+      if (user) {
+        // bad - this is signup, they shouldn't already exist
+        return res.status(409).send({message: 'Email address in use'})
+      }
+        // good - user does not exist
+      db.User.create(req.body)
+        .then(newUser => {
+          // have new user, now need to make them a token
+          let token = jwt.sign(newUser.toJSON(), process.env.JWT_SECRET, {
+            expiresIn: 60 //60*60*8 //8 hours in seconds
+          })
+          // then send token to caller
+          res.send({ token })
+          })
+        .catch(err => {
+         console.log('Error in creating user', err)
+          res.status(500).send({message: 'Error creating user'})
+        })
+      })
+    .catch(err => {
+      console.log('Error in POST/AUTH signup', err)
+      res.status(503).send({message: 'Database or server error'})
+  })
 })
 
 // NOTE: User should be logged in to access this route
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
 // Require needed packages
+let cors = require('cors')
 let express = require('express')
+let expressJwt = require('express-jwt')
 let morgan = require('morgan')
 let rowdyLogger = require('rowdy-logger')
 
@@ -9,9 +12,18 @@ let rowdyResults = rowdyLogger.begin(app)
 
 // Set up middleware
 app.use(morgan('dev'))
+app.use(cors()) //give react app address when available
+app.use(express.urlencoded({extended: false})) // Accept form data
+app.use(express.json()) //accept data from fetch or any AJAX call
 
 // Routes
-app.use('/auth', require('./controllers/auth'))
+app.use('/auth', expressJwt({secret: process.env.JWT_SECRET
+}).unless({ //unless defines exceptions to the rule
+  path: [
+    {url: '/auth/login', methods: ['POST']},
+    {url: 'auth/signup', methods: ['POST']}
+  ]
+}), require('./controllers/auth'))
 
 app.get('*', (req, res) => {
   res.status(404).send({ message: 'Not Found' })
